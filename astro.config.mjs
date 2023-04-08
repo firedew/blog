f10abc7db93662e18c683128377a8434ec51bae8
Swaana <swaanas@gmail.com>
2023-04-08 10:33:58 +0200

    WIP: making it my own

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), mdx()],
+  site: 'https://blog.firedew.eu',
   vite: {
     server: {
       watch: {
