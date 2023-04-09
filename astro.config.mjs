5e28a71f66ab2634c890e678b288f3ac9c82e60f
Swaana <swaanas@gmail.com>
2023-04-09 13:16:18 +0200

    Blogging with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), mdx()],
+  integrations: [
+    vue(),
+    mdx(),
+    tailwind({}),
+    sitemap(),
+  ],
   site: 'https://blog.firedew.eu',
   vite: {
     server: {
